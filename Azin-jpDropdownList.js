448df2fb41d7a4113aa7a4191cab0e75b04d1832
Mojtaba Ghasemi <mojtaba_ghasemi@outlook.com>
2019-10-14 12:57:29 +0330

    Add files via upload

---
+++ Azin-jpDropdownList.js
@@ -0,0 +1,18 @@
+﻿
+(function ($) {
+
+    $.fn.jpDropdownList = function (options) {
+        var container = this;
+        container.find('li>input').click(function (e) {
+            container.attr("value", $(this).attr("value"));
+            container.trigger('onValueChanged', [container.attr("value")]);
+            //if (options.onValueChanged !== undefined) {
+            //    options.onValueChanged( container.attr("value"));
+            //}
+        });
+        return this;
+    };
+
+}(jQuery));
+
+ 
