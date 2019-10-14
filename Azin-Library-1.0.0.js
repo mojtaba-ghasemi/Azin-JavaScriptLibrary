77f4b5a09c8a29f10f32ef8dc522dc1aaecdf318
Mojtaba Ghasemi <mojtaba_ghasemi@outlook.com>
2019-10-14 12:54:32 +0330

    Add files via upload

---
+++ Azin-Library-1.0.0.js
@@ -0,0 +1,6 @@
+﻿function _dbManager_CreateDBBackupAllTables(imgWait, clpanelContainer) {
+ 
+    var valueStream = 'backupDBPathǵ' + BackupDBPath + 'ǴbackupDBFolderNameǵ' + BackupDBFolderName;
+    _postData(AzinCreateDBBackup_Url, '', imgWait, clpanelContainer, null, null, null, valueStream);
+}
+
