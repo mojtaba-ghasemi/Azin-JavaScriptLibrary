50b07c94e9fe7102dc81793758a7a4494c6b2b1d
Mojtaba Ghasemi <mojtaba_ghasemi@outlook.com>
2019-10-14 13:02:22 +0330

    Add files via upload

---
+++ Azin-DialogPanel.js
@@ -0,0 +1,46 @@
+﻿
+function openDialog(params) {
+    try {
+        $(".azinDialogBox .close").click(function (e) {
+            closeDialog();
+        });
+
+        $("body").keypress(function (e) {
+            var key = e.which;
+            if (key == 0) // the enter key code
+            {
+                closeDialog();
+            }
+        });
+
+        var panle = $("body");
+        $(panle).addClass("dialogLoading");
+        
+        //$(".azinDialogBox").show(slow);
+        $(".azinDialogBox").fadeIn(700);
+        ;
+        $(".dialogInputParams").attr("value" , params);
+
+    } catch (e) {
+        alert(e);
+    }
+
+}
+
+function closeDialog() {
+    try {        
+        var panle = $("body");
+        $(".azinDialogBox").hide("slow" , function() {
+            $(panle).removeClass("dialogLoading");
+            $(".dialogInputParams").attr("value", "");
+        });
+    } catch (e) {
+        alert(e);
+    }
+
+}
+
+
+ 
+
+ 
