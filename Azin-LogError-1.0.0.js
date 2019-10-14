ae77b47d78dd990ea77343d94a402196962d181a
Mojtaba Ghasemi <mojtaba_ghasemi@outlook.com>
2019-10-14 12:58:17 +0330

    Add files via upload

---
+++ Azin-LogError-1.0.0.js
@@ -0,0 +1,14 @@
+﻿function logError(filename , functionName , msgtitle ,  msg) {
+ 
+    var error = msgtitle + " . " + msg;
+
+    var _inputstream = "";
+    _inputstream = "FileName" + _streamItemSep + filename;
+    _inputstream += _streamPartSep + "FunctionName" + _streamItemSep + functionName;
+    _inputstream += _streamPartSep + "Message" + _streamItemSep + error;
+
+    $.post(StaticMethodServerUrl, { serviceName: "Com_ClientLogError", inputstream: _inputstream }, function() {
+
+    });
+}
+
