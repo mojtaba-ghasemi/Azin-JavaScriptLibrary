f39235bb914467127d22f9d707ea32c72b988e0b
Mojtaba Ghasemi <mojtaba_ghasemi@outlook.com>
2019-10-14 12:57:51 +0330

    Add files via upload

---
+++ Azin-jpRadionBox.js
@@ -0,0 +1,32 @@
+﻿(function ($) {
+
+    $.fn.jpRadionBox = function (options) {
+
+        var container = this; 
+        this.find('.btn').click(function (e) {
+
+            $(this).closest(".RadioBoxContainer").find('span').removeClass('glyphicon-check');
+            $(this).closest(".RadioBoxContainer").find('span').addClass('glyphicon-unchecked');
+
+            $(this).find('span').removeClass('glyphicon-unchecked');
+            $(this).find('span').addClass('glyphicon-check');
+
+            var selVal = $(this).attr("value");
+            if (typeof selVal !== typeof undefined && selVal !== false) {
+                $(this).closest(".RadioBoxContainer").attr("value", selVal);
+                //if (options !== undefined && options.onValueChanged !== undefined) {
+                //    options.onValueChanged(selVal);
+                //}
+
+                //$(document).trigger('onValueChanged.RadionBoxController', [selVal]);
+
+                container.trigger('onValueChanged', [selVal]);
+
+            }
+        });
+        return this;
+    };
+
+}(jQuery));
+
+
